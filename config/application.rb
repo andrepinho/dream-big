d96dfaa7d0acf0994f2f3d19679c6f2f90dfcd01
André Pinho <andresouzapinho@gmail.com>
2017-01-20 01:18:21 -0200

    Configure generators

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,15 @@ module DreamBigSchool
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :slim
+      g.test_framework  :rspec, fixture: false
+      g.helper          false
+      g.stylesheets     false
+      g.view_specs      false
+      g.javascripts     false
+    end
   end
 end
